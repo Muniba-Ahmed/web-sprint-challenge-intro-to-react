ae0337e61c047b6b6a78225ac663dcd245695de9
Muniba Ahmed <mahmed93@berkeley.edu>
2022-03-24 23:35:10 -0700

    working on rendering

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [jedi, setJedi] = useState([]);
+  const [currentJediId, setCurrentJediId] = useState(null);
+
+  const openDetails = (id) => {
+    setCurrentJediId(id);
+  };
+  const closeDetails = () => {
+    setCurrentJediId(null);
+  };
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios
+      .get(`https://swapi.dev/api/people/`)
+      .then((res) => {
+        console.log(res.data);
+        setJedi(res.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">StarWars Characters</h1>
+      {/* {jedi.map((st) => {
+        console.log(st);
+      })}
+
+       {currentJediId && <Character jedi={jedi} />} */}
     </div>
   );
 };
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,20 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+//add styled components here
+
+const Character = (props) => {
+  const { jedi } = props;
+
+  return (
+    <div className="characterContainer">
+      {/* <h1>My StarWars Peeps</h1>
+      {data.map((char) => {
+        return;
+      })} */}
+    </div>
+  );
+};
+
+export default Character;
